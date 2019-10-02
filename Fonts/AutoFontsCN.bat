36a65c35068acdfc7b0702ca0e76e97fc89a58ba
Typescript <poolk008@qq.com>
2019-10-02 09:50:22 +0800

    init

---
+++ Fonts/AutoFontsCN.bat
@@ -0,0 +1,17 @@
+echo off
+cls
+title WOW字体自动批处理程式
+echo 
+copy 1.ttf ARKai_T.ttf
+copy 1.ttf ARKai_C.ttf
+copy 1.ttf ARHei.ttf
+copy 1.ttf FZXHLJW.ttf
+copy 1.ttf FZXHJW.ttf
+copy 1.ttf FZLBJW.ttf
+copy 1.ttf FZJZJW.ttf
+copy 1.ttf FZBWJW.ttf
+copy 1.ttf FRIZQT__.ttf
+copy 1.ttf ARIALN.ttf
+del 1.ttf
+echo WOW字体自动批处理完成,按任意键退出
+pause
