84b0aa10549ce59543739f497d0e3784cc41aa8d
Typescript <poolk008@qq.com>
2019-10-09 21:47:45 +0800

    用战网登录替代原先的账号密码

--- guaji.sikuli/guaji.py
+++ guaji.sikuli/guaji.py
@@ -10,32 +10,19 @@ while True:
     if exists("1566911317174.png",0):
         doubleClick("1566911337994.png");
         time.sleep(20);
-
-    elif exists("1566914662533.png",0):
-        click(Location(10, 10));
-        time.sleep(20);
     
     elif exists( "1566911490504.png"):
         doubleClick("1566911527418.png");
-    #elif exists( "1566914519679.png",0):
-    #    doubleClick("1566914526618.png");    
         time.sleep(20);
     elif exists("1566912124291.png",0):
         click("1566912173905.png");
         time.sleep(20);
     elif exists("1566912201827.png",0):
-        click(Location(886, 567));
-        time.sleep(1);
-        click(Location(886, 567));
-        time.sleep(1);
-        type("账号");
-        time.sleep(1);
-        click(Location(884, 667));
-        time.sleep(1);
-        type("密码");
-        time.sleep(1);
-        click(Location(969, 760));
-        time.sleep(10);
+        click("1570627425420.png");
+        time.sleep(20)
+    elif exists("1570627544862.png",0):
+        click("1570627537523.png")
+        time.sleep(20)
     else:
         time.sleep(random.randrange(40,80));
         print "我擦"
